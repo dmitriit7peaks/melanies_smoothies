4b9404abfbe9af4185591da3b94cb3a348af54e0
dmitriit7peaks <dmitrii.t@7peakssoftware.com>
2026-02-18 13:46:09 +0700

    Update streamlit_app.py
    pandas #2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
+# Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
@@ -13,20 +14,22 @@ st.write(
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your Smoothie will be:", name_on_order)
 
-# Connect to Snowflake
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-# 1. Pull the data, including the new SEARCH_ON column
+# Pull the data and include the SEARCH_ON column
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+# st.dataframe(data=my_dataframe, use_container_width=True)
+# st.stop()
 
-# 2. Convert to Pandas so we can use it for easy lookups later
+# Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
 pd_df = my_dataframe.to_pandas()
+# st.dataframe(pd_df)
+# st.stop()
 
-# Display the multiselect using the FRUIT_NAME column
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:',
-    my_dataframe, # Using the snowpark df here is fine for the list
+    my_dataframe,
     max_selections=5
 )
 
@@ -36,13 +39,13 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         
-        # 3. Find the SEARCH_ON value for the selected fruit using our Pandas lookup
+        # New logic to get the API search value
         search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
 
         st.subheader(fruit_chosen + ' Nutrition Information')
         
-        # 4. Use the SEARCH_ON variable in the API call instead of fruit_chosen
+        # Use the search_on variable in the API call
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
