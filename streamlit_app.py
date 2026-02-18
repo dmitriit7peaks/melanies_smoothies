2008d0cdf3d77033400b703296c697c5b3fa874a
dmitriit7peaks <dmitrii.t@7peakssoftware.com>
2026-02-18 13:49:38 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,18 +14,15 @@ st.write(
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your Smoothie will be:", name_on_order)
 
+# Connect to Snowflake
 cnx = st.connection("snowflake")
 session = cnx.session()
 
 # Pull the data and include the SEARCH_ON column
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
-# st.dataframe(data=my_dataframe, use_container_width=True)
-# st.stop()
 
 # Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
 pd_df = my_dataframe.to_pandas()
-# st.dataframe(pd_df)
-# st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:',
@@ -39,14 +36,14 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         
-        # New logic to get the API search value
+        # A Strange-Looking Statement That Will Get Us the "Search On" Value
         search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
+        # st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
 
         st.subheader(fruit_chosen + ' Nutrition Information')
         
-        # Use the search_on variable in the API call
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
+        # Use the SEARCH_ON value in the API call using an f-string
+        fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{search_on}")
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     # Build the SQL Insert String
